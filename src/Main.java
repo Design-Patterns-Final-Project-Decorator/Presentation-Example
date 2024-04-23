227aa72a7a7385e39e23f2313229fe25d85c6d70
David Litton <littond@msoe.edu>
2024-04-23 10:54:20 -0500

    added bases javaFX needs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //create an FXMLLoader object so you can get/set your controller objects
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("OpeningPage.fxml"));
+        Parent mainWindow = loader.load();
+        stage.setScene(new Scene(mainWindow));
+        stage.show();
     }
 }
